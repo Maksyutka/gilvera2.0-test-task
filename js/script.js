2bffb03539db613863668b858cc61dff43530436
Maksyutka <maksyutka4579@gmail.com>
2023-02-08 22:45:17 +0200

    scroll-fix

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const mobileMenuToggle = () => {
   $(".burger").click(function () {
     $(".sidebar").toggleClass("sidebar_hidden");
     $(".burger").toggleClass("burger_active");
+    $("html").toggleClass("overflowed");
   });
 };
 
